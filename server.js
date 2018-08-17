a62e6b0d9b71326ce95ac0dd135f69587c662a1a
fatchan <admin@fatpeople.lol>
2018-08-17 17:22:29 +0000

    update login nad logout to POST since its state changing, CSRF protect some things, add helmet (allow iframes for frames in discord bot listing sites)

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const express  = require('express')
 	, socketio = require('socket.io')
 	, app      = express()
 	, server   = require('http').createServer(app)
+	, helmet = require('helmet')
+	, csrf = require('csurf')
+	, bodyParser = require('body-parser')
 
 const config  = require('./configs/main.json');
 const info  = require('./configs/info.json');
@@ -17,15 +20,18 @@ const faq = require('./configs/faq.json');
 const widgets = require('./configs/widgets.json');
 
 async function startServer() {
+	const scopes = ['identify', 'guilds'];
 	const historicalstats = [];
 	const io = socketio(server, { transports: ['websocket'] });
 	io.on('connection', async(socket) => {
-		console.log('New Stats Viewer');
 	    socket.emit('statstart', historicalstats);
 	});
 	const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true })
-    const db = client.db(config.statsDbName).collection('stats');
-	const changeStream = db.watch();
+    const statsDB = client.db(config.statsdbName).collection('stats');
+ 	const gsetDB = client.db(config.statsdbName).collection('gsets');
+	const playlistDB = client.db(config.statsdbName).collection('playlists');
+	const permsDB = client.db(config.statsdbName).collection('permissions');
+	const changeStream = statsDB.watch();
 	changeStream.on("change", (change) => {
 		if (change.operationType === 'insert' || change.operationType === 'replace') {
 			if (change.fullDocument._id === 'stats') {
@@ -39,14 +45,12 @@ async function startServer() {
 			}
 		}
 	});
-
 	passport.serializeUser((user, done) => {
 		done(null, user);
 	});
 	passport.deserializeUser((obj, done) => {
 		done(null, obj);
 	});
-	const scopes = ['identify', 'guilds'];
 	passport.use(new Strategy({
 		clientID: config.passportClientID,
 		clientSecret: config.passportClientSecret,
@@ -57,95 +61,66 @@ async function startServer() {
 			return done(null, profile);
 		});
 	}));
-	app.disable('x-powered-by');
 	app.set("view engine", "pug");
 	app.set("views", path.join(__dirname, "views"));
 	app.use(session({
 		secret: config.sessionSecret,
-		store: new MongoStore({ db: client.db(config.sessionDbName2) }),
-//		store: new MongoStore({ url: config.dbURL2 }),
+		store: new MongoStore({ db: client.db(config.sessionDbName) }),
 		resave: false,
-		saveUninitialized: false
+		saveUninitialized: false,
 	}));
 	app.use(passport.initialize());
 	app.use(passport.session());
-
-	const gsetDB = client.db(config.dbName).collection('gsets');
-	const playlistDB = client.db(config.dbName).collection('playlists');
-	const permsDB = client.db(config.dbName).collection('permissions');
-
-	app.get('/api/guilds/:guildid', checkAuth, async(req, res) => {
-		console.log(req.url, JSON.stringify({id: req.user.id, username: req.user.username}))
-		if (req.params.guildid) {
-			const guild = req.user.guilds.find(guild => guild.id === req.params.guildid);
-			if (!guild) {
-				//prevent sneaky people getting info from servers they are not in
-                return res.status(403).json({error:'Unauthorized. You are not a member of that guild.'});
-            }
-			const settings = await gsetDB.findOne({_id:guild.id});
-			if (!settings) {
-				return res.json({error:'Bot not in this guild.'});
-			}
-			const permissions = await permsDB.findOne({_id:guild.id});
-			const playlists = await playlistDB.findOne({_id:guild.id});
-			res.json({guild:guild, settings:settings.value, playlists:playlists.value, permissions:permissions.value});
-		} else {
-			res.status(400).json({error: 'Invalid request.'});
-		}
-	});
-
-	app.get('/robots.txt', (req, res) => {
-		res.type('text/plain');
-		res.send("User-agent: *\nDisallow: /");
-	});
-	app.get('/login',
-		passport.authenticate('discord', { scope: scopes }),
-		(req, res) => {}
-	);
-	app.get('/callback',
-		passport.authenticate('discord', { failureRedirect: '/' }),
-		(req, res) => { res.redirect('/dashboard') } // auth success
-	);
-	app.get('/logout', (req, res) => {
-		req.logout();
-		res.redirect('/');
-	});
+	app.use(bodyParser.json())
+	app.use(bodyParser.urlencoded({
+		extended: false
+	}))
+	app.use(helmet({
+		frameguard: false //allow for discord bot listing sites
+	}))
+	app.use(csrf({
+		ignoreMethods: ['GET', 'HEAD', 'OPTIONS']
+	}))
 	app.get('/', (req, res) => {
-		//console.log(req.user)
 		res.render('homepage', {
 			cache: true,
 			user: req.user,
 			configs: info,
 			commands: commands,
-			widgets: widgets
+			widgets: widgets,
+			csrf: req.csrfToken()
 		});
 	});
 	app.get('/dashboard', checkAuth, (req, res) => {
-		//console.log(req.user)
 		res.render('dashboard', {
 			cache: true,
 			configs: info,
-			user: req.user
+			user: req.user,
+			csrf: req.csrfToken()
 		});
 	});
 	app.get('/stats', (req, res) => {
-		//console.log(req.user)
 		res.render('stats', {
 			cache: true,
 			configs: info,
-			user: req.user
+			user: req.user,
+			csrf: req.csrfToken()
 		});
 	});
 	app.get('/faq', (req, res) => {
-		//console.log(req.user)
 		res.render('faq', {
 			cache: true,
 			user: req.user,
 			configs: info,
 			faq: faq,
-			widgets: widgets
+			widgets: widgets,
+			csrf: req.csrfToken()
 		});
 	});
+	app.get('/callback',
+		passport.authenticate('discord', { failureRedirect: '/' }),
+		(req, res) => { res.redirect('/dashboard') } // auth success
+	);
 	app.get('/vote', (req, res) => {
 		res.redirect(info.voteURL);
 	});
@@ -158,16 +133,66 @@ async function startServer() {
 	app.get('/github', (req, res) => {
 		res.redirect(info.githubURL);
 	});
+	app.get('/robots.txt', (req, res) => {
+		res.type('text/plain');
+		res.send('User-agent: *\nDisallow: /');
+	});
+	app.get('/api/guilds/:guildid', checkAuth, dashboardGuildCheck, async(req, res) => {
+		res.locals.permissions = await permsDB.findOne({_id:res.locals.guildid});
+		res.locals.playlists = await playlistDB.findOne({_id:res.locals.guildid});
+		res.json({guild:res.locals.guild, settings:res.locals.settings.value, playlists:res.locals.playlists.value, permissions:res.locals.permissions.value});
+	});
 	app.get('*', (req, res) => {
 		res.status(404).render('404', {
 			cache: true,
 			configs: info,
-			user: req.user
+			user: req.user,
+			csrf: req.csrfToken()
 		});
 	});
+	app.post('/logout', checkAuth, (req, res) => {
+		req.logout();
+		res.redirect('/');
+	});
+	app.post('/login',
+		passport.authenticate('discord', { scope: scopes }),
+		(req, res) => {}
+	);
+    app.post('/api/guilds/:guildid/edit', checkAuth, dashboardGuildCheck, async(req, res) => {
+		//i guess accept JSON payload with the updated json. gonna have to chek all that data to be valid, probably with another mniddleware
+		res.json({error:'Not implemented yet.'});
+    });
+	app.use((err, req, res, next) => {
+		if (err.code !== 'EBADCSRFTOKEN') return next(err);
+		res.status(403);
+		res.send('HALT!'); //invalid csrf token
+	})
 	function checkAuth(req, res, next) {
-		if (req.isAuthenticated()) return next();
-		res.redirect('/login')
+		if (req.isAuthenticated()) {
+			return next();
+		} else {
+			res.redirect('/login');
+		}
+	}
+	async function dashboardGuildCheck(req, res, next) {
+        if (req.params.guildid) {
+            const guild = req.user.guilds.find(guild => guild.id === req.params.guildid);
+            if (!guild) {
+                return res.status(403).json({error:'Unauthorized. You are not a member of that guild.'});
+            }
+			res.locals.guildid = guild.id
+			try {
+	            res.locals.settings = await gsetDB.findOne({_id:res.locals.guildid});
+			} catch(e) {
+				return res.json({error:'Error reading from DB'});
+			}
+            if (!res.locals.settings) {
+                return res.json({error:'Bot not in this guild.', });
+            }
+			next();
+        } else {
+            res.status(400).json({error: 'Invalid request.'});
+        }
 	}
 	server.listen(config.port);
 	console.log('Listening on port '+config.port);
--- views/api.js
+++ views/api.js
@@ -1,20 +1,24 @@
 <script>
 	$( document ).ready(function() {
+
+		//getting info for a server
 		$('.sidebar-icon-radius').click(function() {
 			var ID = $(this).attr('data-attribute');
-			$.ajax({ 
-				type: 'GET', 
-				url: '/api/guilds/'+ID, 
+			var token = $('#csrf').attr('value');  //use this for POSTS in other requests
+			$.ajax({
+				type: 'GET',
+				url: '/api/guilds/'+ID,
 				dataType: 'json',
 				success: function (data) {
 					$('.server-info').empty();
 					if (!data) { return; }
-					console.log(JSON.stringify(data, null, 2))
+					//console.log(data)
 					$('.server-info').append(
 						$('<pre>').text(JSON.stringify(data, null, 2))
 					);
 				}
 			});
 		});
+
 	});
 </script>
