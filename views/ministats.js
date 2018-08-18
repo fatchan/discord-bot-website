67d1953fcfccad5a2e1deecb7f97e480c7f4157f
fatchan <admin@fatpeople.lol>
2018-08-18 11:30:14 +0000

    red/green flash stats on homepage as they change

--- views/ministats.js
+++ views/ministats.js
@@ -8,9 +8,29 @@
 	socket.on('stats', function(data) {
 		updatechart(data, true);
 	});
+	var flash = function(elements, color) {
+		var opacity = 50;
+		var interval = setInterval(function() {
+			opacity -= 3;
+			if (opacity <= 0) clearInterval(interval);
+			$(elements).css({background: "rgba("+color+", "+opacity/100+")"});
+		}, 30)
+	};
 	var updatechart = function(data) {
-		$('#stats-guilds').text(data.guilds);
-		$('#stats-users').text(data.users);
-		$('#stats-streams').text(data.totalVoiceConnections);
-	}
+		if (data.guilds != $('#stats-guilds').text()) {
+			var guildsColor = data.guilds < $('#stats-guilds').text() ? '150, 0, 0' : '0, 150, 0';
+			$('#stats-guilds').text(data.guilds);
+			flash('#stats-guilds', guildsColor);
+		}
+		if (data.users != $('#stats-users').text()) {
+			var usersColor = data.users < $('#stats-users').text() ? '150, 0, 0' : '0, 150, 0';
+			$('#stats-users').text(data.users);
+			flash('#stats-users', usersColor);
+		}
+		if (data.totalVoiceConnections != $('#stats-streams').text()) {
+			var streamsColor = data.totalVoiceConnections < $('#stats-streams').text() ? '150, 0, 0' : '0, 150, 0';
+			$('#stats-streams').text(data.totalVoiceConnections);
+			flash('#stats-streams', streamsColor);
+		}
+	};
 </script>
