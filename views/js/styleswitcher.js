5760e50531fdb07f9b9e14c5294bd007e875c676
fatchan <admin@fatpeople.lol>
2018-08-24 16:11:48 +0000

    fix styleswitching double click bug and jquery cookie

--- views/js/styleswitcher.js
+++ views/js/styleswitcher.js
@@ -6,61 +6,14 @@ function setActiveStyleSheet(title) {
       a.disabled = true;
       if(a.getAttribute("title") == title) {
         a.disabled = false;
-		if(typeof chart != 'undefined')
-			newColor(a.getAttribute('chartcolor'))
-        createCookie("style", title, 365);
+		$.cookie("style", title, { path: '/', expires: 7 });
       }
     }
   }
 }
 
-function getActiveStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
-  }
-  return null;
-}
-
-function getPreferredStyleSheet() {
-  var i, a;
-  for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-    if(a.getAttribute("rel").indexOf("style") != -1
-       && a.getAttribute("rel").indexOf("alt") == -1
-       && a.getAttribute("title")
-       ) return a.getAttribute("title");
-  }
-  return null;
-}
-
-function createCookie(name,value,days) {
-  if (days) {
-    var date = new Date();
-    date.setTime(date.getTime()+(days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
-  }
-  else expires = "";
-  document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function readCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for(var i=0;i < ca.length;i++) {
-    var c = ca[i];
-    while (c.charAt(0)==' ') c = c.substring(1,c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-  }
-  return null;
-}
-
-function saveCookie() {
-  var title = getActiveStyleSheet();
-  createCookie("style", title, 365);
-}
-
 function togglestyle() {
-	var title = getActiveStyleSheet();
+	var title = $.cookie("style");
 	if(title == 'light') {
 		setActiveStyleSheet('dark')
 	} else {
@@ -69,7 +22,13 @@ function togglestyle() {
 }
 
 $( document ).ready(function() {
-	setActiveStyleSheet(getActiveStyleSheet());
+	if ($('#chartContainer1').length >= 1 && $.cookie("style")) {
+		if ($.cookie("style") == 'dark') {
+			newColor('#fff');
+		} else {
+			newColor('#000');
+		}
+	}
 	var button = document.getElementById('styleswitcher');
 	button.onclick = function() {
 		togglestyle();
