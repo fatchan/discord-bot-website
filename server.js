42a60a45d7488f222575e2b9b0c63a8623d58534
fatchan <admin@fatpeople.lol>
2018-10-23 03:40:04 +0000

    serve static files in nginx instead of express

--- server.js
+++ server.js
@@ -51,9 +51,7 @@ module.exports = (app, client, config) => {
 		//allow for discord bot listing sites
 		frameguard: false
 	}))
-	app.use('/static/css', express.static(__dirname + '/views/css'));
-	app.use('/static/js', express.static(__dirname + '/views/js'));
-	app.use('/static/img', express.static(__dirname + '/views/img'));
+
 	app.use(morgan(':method HTTP/:http-version :url Status::status Content-Length::res[content-length] :remote-addr ":referrer" ":user-agent"'))
 
 	//setup routes
