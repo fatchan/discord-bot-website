9be11e7f136dcbcbf16d0d43668e322436936c7d
fatchan <admin@fatpeople.lol>
2018-11-10 08:59:24 +0000

    idiomatic

--- launch.js
+++ launch.js
@@ -7,21 +7,21 @@ const express = require('express')
 	, { MongoClient } = require('mongodb')
 	, config  = require('./configs/main.json')
 
-MongoClient.connect(config.dbURL, { useNewUrlParser: true }, async(err, client) => {
-	if (err) {
-		console.error(err);
-        process.exit(1);
-	}
+async function launch() {
+	const client = await MongoClient.connect(config.dbURL, { useNewUrlParser: true });
 	const app = express();
 	const server = require('http').createServer(app);
 	const website = require('./server.js')(app, client, config);
-	try {
-		const websocket = await require('./websocket.js').start(server, client, config);
-	} catch(err) {
-		console.error(err);
-		process.exit(1);
-	}
+	const websocket = require('./websocket.js');
+	await websocket.start(server, client, config);
 	server.listen(config.port, () => {
         console.log('Listening on port '+config.port);
     });
-});
+}
+
+try {
+	launch();
+} catch(e) {
+	console.error(e);
+	process.exit(1);
+}
