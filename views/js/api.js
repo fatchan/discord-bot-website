7d9281f2f6165bd7501fe94c2f403e2886c6a53b
fatchan <tlyn6143@uni.sydney.edu.au>
2018-11-15 03:04:53 -0500

    navbar to match

--- views/js/api.js
+++ views/js/api.js
@@ -1,27 +1,24 @@
-<script type="text/javascript">
-	$( document ).ready(function() {
-
-		//getting info for a server
-		$('.sidebar-icon-radius').click(function() {
-			var ID = $(this).attr('data-attribute');
-			var token = $('#csrf').attr('value');  //use this for POSTS in other requests
-			$.ajax({
-				type: 'GET',
-				url: '/api/guilds/'+ID,
-				dataType: 'json',
-				success: function (data) {
-					$('.server-info').empty();
-					if (!data) { return; }
-					//console.log(data)
-					$.each(Object.keys(data.settings), function (i, item) {
-						if(data.settings[item]) {
-			                var setting = ('<ul>' + item + ': ' + data.settings[item] + '</ul>');
-			                $('.server-info').append(setting);
-						}
-		            });
-				}
-			});
+$( document ).ready(function() {
+	//getting info for a server
+	$('.sidebar-icon-radius').click(function() {
+		var ID = $(this).attr('data-attribute');
+		var token = $('#csrf').attr('value');  //use this for POSTS in other requests
+		$.ajax({
+			type: 'GET',
+			url: '/api/guilds/'+ID,
+			dataType: 'json',
+			success: function (data) {
+				$('.server-info').empty();
+				if (!data) { return; }
+				//console.log(data)
+				var anydata = data.error ? data : data.settings
+				$.each(Object.keys(anydata), function (i, item) {
+					if(anydata[item]) {
+		                var setting = ('<ul>' + item + ': ' + anydata[item] + '</ul>');
+		                $('.server-info').append(setting);
+					}
+	            });
+			}
 		});
-
 	});
-</script>
+});
