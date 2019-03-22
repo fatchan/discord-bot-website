e70457bcdcc4c46b321c11b4a85d641ea25fe6c5
fatchan <tom@69420.me>
2019-03-22 23:59:29 +0000

    simpler version minus unused bloat in master. will use master if i ever implement a dashboard

--- src/routes/pages.js
+++ src/routes/pages.js
@@ -10,20 +10,11 @@ const express = require('express')
 
 module.exports = () => {
 
-	/*
-	TODO: actually implement this
-    const gsetDB = Mongo.client.db(config.statsdbName).collection('gsets');
-    const playlistDB = Mongo.client.db(config.statsdbName).collection('playlists');
-    const permsDB = Mongo.client.db(config.statsdbName).collection('permissions');
-	*/
-
     router.get('/', (req, res) => {
         res.render('homepage', {
             cache: false,
             style: req.cookies.style || config.defaultStyle,
-            user: req.user,
             configs: info,
-            csrf: req.csrfToken(),
             stats: Stats.tombot
         });
     });
@@ -35,17 +26,6 @@ module.exports = () => {
             user: req.user,
             configs: info,
             commands: commands,
-            csrf: req.csrfToken()
-        });
-    });
-	
-    router.get('/dashboard', checkAuth, (req, res) => {
-        res.render('dashboard', {
-            cache: true,
-            style: req.cookies.style || config.defaultStyle,
-            configs: info,
-            user: req.user,
-            csrf: req.csrfToken()
         });
     });
 	
@@ -57,10 +37,8 @@ module.exports = () => {
         res.render('faq', {
             cache: true,
             style: req.cookies.style || config.defaultStyle,
-            user: req.user,
             configs: info,
             faq: faq,
-            csrf: req.csrfToken()
         });
     });
 	
@@ -68,16 +46,9 @@ module.exports = () => {
         res.render('vip', {
             cache: true,
             style: req.cookies.style || config.defaultStyle,
-            user: req.user,
             configs: info,
-            csrf: req.csrfToken()
         });
     });
-	
-    router.get('/callback',
-        passport.authenticate('discord', { failureRedirect: '/' }),
-        (req, res) => { res.redirect('/dashboard') } // auth success
-    );
 	
     router.get('/vote', (req, res) => {
         res.redirect(info.voteURL);
@@ -104,61 +75,13 @@ module.exports = () => {
         res.send('User-agent: *\nDisallow:');
     });
 	
-	/*	
-	router.get('/api/guilds/:guildid', dashboardGuildCheck, (req, res) => {
-		res.json({guild:res.locals.guild, settings:res.locals.settings.value, permissions:res.locals.permissions.value});
-	})
-	*/
-	
-	router.get('/login',
-        passport.authenticate('discord', { scope: ['identify', 'guilds'] }),
-        (req, res) => {}
-    );
-	
     router.get('*', (req, res) => {
         res.status(404).render('404', {
             cache: true,
             style: req.cookies.style || config.defaultStyle,
-            user: req.user,
             configs: info,
-            csrf: req.csrfToken()
         });
     });
-	
-    router.post('/logout', checkAuth, (req, res) => {
-        req.logout();
-        res.redirect('/');
-    });
-	
-	function checkAuth(req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			return res.redirect('/login')
-		}
-	}
-	
-	async function dashboardGuildCheck(req, res, next) {
-        if (req.params.guildid) {
-            const guild = req.user.guilds.find(guild => guild.id === req.params.guildid);
-            if (!guild) {
-                return res.status(403).json({error:'Unauthorized. You are not a member of that guild.'});
-            }
-			res.locals.guildid = guild.id
-			try {
-	            res.locals.settings = await gsetDB.findOne({_id:res.locals.guildid});
-			} catch(e) {
-				return res.json({error:'Error reading from DB'});
-			}
-            if (!res.locals.settings) {
-                return res.json({error:'Bot not in this guild.', });
-            }
-			res.locals.permissions = await permsDB.findOne({_id:res.locals.guildid});
-			next();
-        } else {
-            res.status(400).json({error: 'Invalid request.'});
-        }
-	}
 
 	return router;
 
